6e806eb88551144354f2f39b6d44d05aadf4fa1e
lalitha2407 <lalitha@bootlabstech.com>
2023-03-30 22:43:28 +0530

    rewritten module

--- main.tf
+++ main.tf
@@ -1,59 +1,35 @@
-resource "azurerm_virtual_machine" "virtual_machine" {
+
+resource "azurerm_windows_virtual_machine" "example" {
   name                             = var.name
-  location                         = var.location
   resource_group_name              = var.resource_group_name
+  location                         = var.location
+  size                             = var.size
+  admin_username                   = var.admin_username
+  admin_password                   = var.admin_password
   network_interface_ids            = [azurerm_network_interface.network_interface.id]
-  vm_size                          = var.vm_size
-  delete_os_disk_on_termination    = var.delete_os_disk_on_termination
-  delete_data_disks_on_termination = var.delete_data_disks_on_termination
+  license_type                     = var.license_type  
 
-  storage_image_reference {
-    publisher = var.publisher
-    offer     = var.offer
-    sku       = var.sku
-    version   = var.storage_image_version
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    name                 = "${var.name}-disk"
+    caching              = "ReadWrite"
+    storage_account_type =  var.storage_account_type #"Standard_LRS"
+    disk_size_gb = var.disk_size_gb
   }
 
-  storage_os_disk {
-    name              = "${var.name}-disk"
-    caching           = var.caching
-    create_option     = var.create_option
-    managed_disk_type = var.managed_disk_type
-    os_type           = var.os_type
+  source_image_reference {
+    publisher = var.publisher #MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:20348.1006.220908
+    offer     = var.offer  #MicrosoftWindowsServer:WindowsServer:2019-datacenter-core-g2:17763.2686.220303
+    sku       = var.sku #MicrosoftWindowsServer:WindowsServer:2016-datacenter-gensecond:14393.5006.220305
+    version   = var.version
   }
-
-  os_profile {
-    computer_name  = var.name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-    custom_data    = var.custom_data
-  }
-
-  dynamic "os_profile_linux_config" {
-    for_each = var.os_type == "Linux" ? [1] : []
-    content {
-      disable_password_authentication = var.disable_password_authentication
-    }
-  }
-
-  dynamic "os_profile_windows_config" {
-    for_each = var.os_type == "Windows" ? [1] : []
-    content {
-      timezone = var.timezone
-      provision_vm_agent = true
-    }
-    
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
-  depends_on = [
+   depends_on = [
     azurerm_network_interface.network_interface
   ]
 }
+
 resource "azurerm_network_interface" "network_interface" {
   name                = "{{.name}}-nic"
   location            = var.location
@@ -67,8 +43,8 @@ resource "azurerm_network_interface" "network_interface" {
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "{{.name}}-nsg"
-  location            = azurerm_virtual_machine.virtual_machine.location
-  resource_group_name = azurerm_virtual_machine.virtual_machine.resource_group_name
+  location            = azurerm_windows_virtual_machine.example.location
+  resource_group_name = azurerm_windows_virtual_machine.example.resource_group_name
 }
 
 resource "azurerm_network_security_rule" "nsg_rules" {
@@ -83,7 +59,7 @@ resource "azurerm_network_security_rule" "nsg_rules" {
   destination_address_prefix  = each.value.destination_address_prefix
   destination_port_range      = each.value.destination_port_range
   network_security_group_name = azurerm_network_security_group.nsg.name
-  resource_group_name         = azurerm_virtual_machine.virtual_machine.resource_group_name
+  resource_group_name         = azurerm_windows_virtual_machine.example.resource_group_name
 }
 
 resource "azurerm_network_interface_security_group_association" "security_group_association" {
@@ -118,3 +94,4 @@ resource "azurerm_backup_protected_vm" "backup_protected_vm" {
     azurerm_backup_policy_vm.backup_policy_vm
   ]
 }
+
--- variables.tf
+++ variables.tf
@@ -16,62 +16,11 @@ variable "location" {
 
 
 
-variable "vm_size" {
+variable "size" {
   type        = string
   description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
 }
 
-# storage_image_reference
-variable "publisher" {
-  type        = string
-  description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
-  default = "MicrosoftWindowsServer"
-}
-
-variable "offer" {
-  type        = string
-  description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
-  default = "WindowsServer"
-}
-
-variable "sku" {
-  type        = string
-  description = "Specifies the SKU of the image used to create the virtual machine. Examples: 18.04-LTS, 2019-Datacenter"
-}
-
-variable "storage_image_version" {
-  type        = string
-  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
-  default     = "latest"
-}
-
-
-# storage_os_disk
-variable "caching" {
-  type        = string
-  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
-  default     = "ReadWrite"
-}
-
-variable "create_option" {
-  type        = string
-  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
-  default     = "FromImage"
-}
-
-variable "managed_disk_type" {
-  type        = string
-  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
-  default = "Standard_LRS"
-}
-
-variable "os_type" {
-  type        = string
-  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
-  default = "Windows"
-}
-
-# os_profile
 variable "admin_username" {
   type        = string
   description = "Specifies the name of the local administrator account."
@@ -82,16 +31,45 @@ variable "admin_password" {
   description = "The password associated with the local administrator account."
 }
 
-variable "custom_data" {
+# os_disk
+variable "storage_account_type" {
   type        = string
-  description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
+  description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
+  default = "Standard_LRS"
 }
-# network_interface
-# variable "network_interface_name" {
-#   type        = string
-#   description = "The name of the Network Interface. Changing this forces a new resource to be created."
-# }
 
+variable "disk_size_gb" {
+  type        = string
+  description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
+  default = "WindowsServer"
+}
+
+# source_image_reference
+variable "publisher" {
+  type        = string
+  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "MicrosoftWindowsServer"
+}
+
+variable "offer" {
+  type        = string
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  default     = "WindowsServer"
+}
+
+variable "sku" {
+  type        = string
+  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+
+}
+
+variable "version" {
+  type        = string
+  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+  default = "Windows"
+}
+
+# azurerm_network_interface
 variable "ip_name" {
   type        = string
   description = "A name used for this IP Configuration."
@@ -108,35 +86,6 @@ variable "private_ip_address_allocation" {
   description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
   default = "Dynamic"
 }
-
-variable "disable_password_authentication" {
-  type        = bool
-  description = "Specifies whether password authentication should be disabled. If set to false, an admin_password must be specified."
-  default     = false
-}
-
-variable "timezone" {
-  type        = string
-  description = "(optional) describe your variable"
-  default     = "India Standard Time"
-}
-
-variable "delete_os_disk_on_termination" {
-  type        = bool
-  description = " Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
-  default     = true
-}
-
-variable "delete_data_disks_on_termination" {
-  type        = bool
-  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
-  default     = true
-}
-
-# variable "nsg_name" {
-#   type        = string
-#   description = "name of the azurerm_network_security_group"
-# }
 
 variable "nsg_rules" {
   type = map(object({
@@ -174,8 +123,8 @@ variable "services_vault_resource_group_name" {
   description = "name of the azurerm_network_security_group"
 }
 
-# variable "policy_name" {
-#   type        = string
-#   description = "name of the azurerm_network_security_group"
-# }
+variable "license_type" {
+  type        = string
+  description = "name of the azurerm_network_security_group"
+}
 
