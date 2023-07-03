91039255af56d151602e657c4e791c0ef99c6642
lalitha2407 <lalitha@bootlabstech.com>
2023-07-03 12:33:14 +0530

    code clean up and added random password string resource

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# Creates a Virtual Machine
+# Creates a Azure Windows Virtual machine
 resource "azurerm_windows_virtual_machine" "example" {
-  name                             = var.name
-  resource_group_name              = var.resource_group_name
-  location                         = var.location
-  size                             = var.size
-  admin_username                   = var.admin_username
-  admin_password                   = random_password.password.result
-  network_interface_ids            = [azurerm_network_interface.network_interface.id]
-  license_type                     = var.license_type  
+  name                  = var.name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = random_password.password.result
+  network_interface_ids = [azurerm_network_interface.network_interface.id]
+  license_type          = var.license_type
 
   identity {
     type = "SystemAssigned"
@@ -15,20 +15,22 @@ resource "azurerm_windows_virtual_machine" "example" {
   os_disk {
     name                 = "${var.name}-disk"
     caching              = "ReadWrite"
-    storage_account_type =  var.storage_account_type #"Standard_LRS"
-    disk_size_gb = var.disk_size_gb
+    storage_account_type = var.storage_account_type
+    disk_size_gb         = var.disk_size_gb
   }
 
   source_image_reference {
-    publisher = var.publisher #MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:20348.1006.220908
-    offer     = var.offer  #MicrosoftWindowsServer:WindowsServer:2019-datacenter-core-g2:17763.2686.220303
-    sku       = var.sku #MicrosoftWindowsServer:WindowsServer:2016-datacenter-gensecond:14393.5006.220305
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
     version   = var.storage_image_version
   }
-   depends_on = [
+  depends_on = [
     azurerm_network_interface.network_interface
   ]
 }
+
+
 # Creates Network Interface Card with private IP for Virtual Machine
 resource "azurerm_network_interface" "network_interface" {
   name                = "${var.name}-nic"
@@ -40,14 +42,17 @@ resource "azurerm_network_interface" "network_interface" {
     private_ip_address_allocation = var.private_ip_address_allocation
   }
 }
+
+
 # Creates Network Security Group NSG for Virtual Machine
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.name}-nic"
+  name                = "${var.name}-nsg"
   location            = azurerm_windows_virtual_machine.example.location
   resource_group_name = azurerm_windows_virtual_machine.example.resource_group_name
 }
-# Creates Network Security Group Default Rules for Virtual Machine
 
+
+# Creates Network Security Group Default Rules for Virtual Machine
 resource "azurerm_network_security_rule" "nsg_rules" {
   for_each                    = var.nsg_rules
   name                        = each.value.name
@@ -62,33 +67,38 @@ resource "azurerm_network_security_rule" "nsg_rules" {
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = azurerm_windows_virtual_machine.example.resource_group_name
 }
+
+
 # Creates association (i.e) adds NSG to the NIC
 resource "azurerm_network_interface_security_group_association" "security_group_association" {
   network_interface_id      = azurerm_network_interface.network_interface.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Getting existing recovery_services_vault to add vm as a backup item 
-data "azurerm_recovery_services_vault" "services_vault" {
-  name                = var.recovery_services_vault_name
-  resource_group_name = var.services_vault_resource_group_name
-}
-# Getting existing Backup Policy for Virtual Machine
-data "azurerm_backup_policy_vm" "policy" {
-  name                = "VM-backup-policy"
-  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
-  resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
-}
-# Creates Backup protected Virtual Machine
-resource "azurerm_backup_protected_vm" "backup_protected_vm" {
-  resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
-  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
-  source_vm_id        = azurerm_windows_virtual_machine.example.id
-  backup_policy_id    = data.azurerm_backup_policy_vm.policy.id
-  depends_on = [
-    azurerm_windows_virtual_machine.example
-  ]
-}
+
+# # Getting existing recovery_services_vault to add vm as a backup item 
+# data "azurerm_recovery_services_vault" "services_vault" {
+#   name                = var.recovery_services_vault_name
+#   resource_group_name = var.services_vault_resource_group_name
+# }
+# # Getting existing Backup Policy for Virtual Machine
+# data "azurerm_backup_policy_vm" "policy" {
+#   name                = "VM-backup-policy"
+#   recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+#   resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
+# }
+# # Creates Backup protected Virtual Machine
+# resource "azurerm_backup_protected_vm" "backup_protected_vm" {
+#   resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
+#   recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+#   source_vm_id        = azurerm_windows_virtual_machine.example.id
+#   backup_policy_id    = data.azurerm_backup_policy_vm.policy.id
+#   depends_on = [
+#     azurerm_windows_virtual_machine.example
+#   ]
+# }
+
+
 # Extention for startup ELK script
 resource "azurerm_virtual_machine_extension" "example" {
   name                 = "${var.name}-elkscript"
@@ -104,16 +114,19 @@ resource "azurerm_virtual_machine_extension" "example" {
     }
 SETTINGS
 }
+
+
+# Creates a random string password for vm default user
 resource "random_password" "password" {
-    length = 8
-    lower = true
-    min_lower = 3
-    min_numeric= 2
-    min_special= 1
-    min_upper= 2
-    numeric = true
-    special = true
-    upper = true
-    
+  length      = 12
+  lower       = true
+  min_lower   = 6
+  min_numeric = 2
+  min_special = 2
+  min_upper   = 2
+  numeric     = true
+  special     = true
+  upper       = true
+
 
 }
--- variables.tf
+++ variables.tf
@@ -1,71 +1,71 @@
 # virtual_machine
 variable "name" {
   type        = string
-  description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
+  description = "Name for the virtual machine. Must not contain uppercase maximum of 15 characters length"
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "name of the resource group"
+  description = "Name of the resource group inside which the VM will be created in"
 }
 
 variable "location" {
   type        = string
-  description = "location of the resource group"
+  description = "Location of the resource group. Must be one of following, Central India or South India"
 }
 
-
-
-variable "size" {
+variable "vm_size" {
   type        = string
-  description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+  description = "Specifies the size of the Virtual Machine based on number of core and RAM.Refer documentation for all options"
 }
 
 variable "admin_username" {
   type        = string
   description = "Specifies the name of the local administrator account."
+  default     = "user01"
 }
-
 
 variable "license_type" {
   type        = string
-  description = "The pass"
-  
+  description = "The License type for Windows VM . Must be one of the values [None, Windows_Client, Windows_Server]"
+
 }
+
 # os_disk
 variable "storage_account_type" {
   type        = string
-  description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
-  default = "Standard_LRS"
+  description = "The Type of Storage Account which should back this the Internal OS Disk. Possible values are Standard_LRS, StandardSSD_LRS, Premium_LRS, StandardSSD_ZRS and Premium_ZRS"
+  default     = "Standard_LRS"
 }
 
 variable "disk_size_gb" {
   type        = string
-  description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
+  description = "The Size of the Internal OS Disk in GB, if you wish to vary from the size used in the image this Virtual Machine is sourced from."
+
 }
 
 # source_image_reference
 variable "publisher" {
   type        = string
-  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  description = "Specifies the Publisher of the Marketplace Image this Virtual Machine should be created from.View documentation for all options"
   default     = "MicrosoftWindowsServer"
 }
 
 variable "offer" {
   type        = string
-  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  description = " Specifies the offer of the image used to create the virtual machines.View documentation for all options "
   default     = "WindowsServer"
 }
 
 variable "sku" {
   type        = string
-  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+  description = "Specifies the SKU of the image used to create the virtual machines.View documentation for all options"
 
 }
 
 variable "storage_image_version" {
   type        = string
-  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+  description = "Specifies the Operating System version on the OS Disk. View documentation for all options"
 
 }
 
@@ -79,14 +79,15 @@ variable "ip_name" {
 
 variable "subnet_id" {
   type        = string
-  description = "The ID of the Subnet where this Network Interface should be located in."
+  description = "The ID of the Subnet where this Network Interface should be located in. Select subnet according to factors 1.application server (appsubnet) 2. web server (websubnet) 3. database server (dbsubnet)"
 }
 
 variable "private_ip_address_allocation" {
   type        = string
   description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
-  default = "Dynamic"
+  default     = "Dynamic"
 }
+
 # azurerm_network_security_rule
 variable "nsg_rules" {
   type = map(object({
@@ -114,12 +115,13 @@ variable "nsg_rules" {
     }
   }
 }
-# azurerm_recovery_services_vault
-variable "recovery_services_vault_name" {
-  type        = string
-  description = "name of the azurerm_network_security_group"
-}
-variable "services_vault_resource_group_name" {
-  type        = string
-  description = "name of the azurerm_network_security_group"
-}
+
+# # azurerm_recovery_services_vault
+# variable "recovery_services_vault_name" {
+#   type        = string
+#   description = "name of the recover service vault"
+# }
+# variable "services_vault_resource_group_name" {
+#   type        = string
+#   description = "name of resource group where the recovery service vault reside in"
+# }
